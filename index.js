d020e2efc94d429c7be8d5b4b27e93ce230e93d8
Nayamat ullah chowdhury Nur <65952839+Nur-Chowdhury@users.noreply.github.com>
2025-07-04 19:17:44 +0000

    dns lookup added

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.use(express.urlencoded({ extended: false }));
+
+let urlDatabase = {};
+let counter = 1;
+
+app.post("/api/shorturl", (req, res) => {
+  const inputUrl = req.body.url;
+
+  try {
+    const urlObj = new URL(inputUrl);
+
+    dns.lookup(urlObj.hostname, (err) => {
+      if (err) return res.json({ error: "invalid url" });
+
+      const short = counter++;
+      urlDatabase[short] = inputUrl;
+
+      res.json({
+        original_url: inputUrl,
+        short_url: short,
+      });
+    });
+  } catch {
+    res.json({ error: "invalid url" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
